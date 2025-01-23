ccadd0d81dce2819102b06f4db34ea1966bc63d6
Mugabe-Joshua <jshmgb3@gmail.com>
2025-01-23 18:49:41 +0300

    FEAT:Pyhton Lesson Two

---
+++ 23rd Jan/me.py
@@ -0,0 +1,21 @@
+# my_name = input("Enter your name: ")
+# print(type(my_name))
+# my_age = input(int('Enter your age: '))
+# print(type(my_age))
+# my_weight = input(float("Enter your weight: "))
+# print(type(my_weight))
+# my_height = input(float("Enter your height: "))
+# print(type(my_height))
+
+my_name = "Joshua"
+my_age = 25
+my_weight = 68.2
+my_height = 172.5
+is_male = True
+print(type(my_name))
+print(type(my_age))
+print(type(my_weight))
+print(type(my_height))
+print(type(is_male))
+#ASSIGNMENT COMPLETE! :)
+
